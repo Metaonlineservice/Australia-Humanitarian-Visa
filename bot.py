ea660eee2ea81c4c40c12dc07eb71d452833c7e1
Metaonlineservice <295571404+Metaonlineservice@users.noreply.github.com>
2026-07-23 03:39:56 +0330

    Add Telegram bot with start command handler

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+
+TOKEN = os.getenv("TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🇦🇺 به ربات معلومات ویزای استرالیا خوش آمدید\n\n"
+        "1️⃣ ویزای بشردوستانه\n"
+        "2️⃣ ویزای کاری\n"
+        "3️⃣ ویزای تحصیلی"
+    )
+
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+
+app.run_polling()
